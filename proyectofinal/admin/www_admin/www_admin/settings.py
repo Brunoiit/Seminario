7ce0ec2d8bb79e4a013752cafb19a270acda070a
Brunoiit <francisco18car@gmail.com>
2023-05-28 21:45:23 -0500

    Modificacion Settings

--- proyectofinal/admin/www_admin/www_admin/settings.py
+++ proyectofinal/admin/www_admin/www_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*etc4jkhyga3-cn*58$wz4wvyrqoc**@=!#i+bozwmd_)#bk9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'panel',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Americaa/Bogota'
 
 USE_I18N = True
 
