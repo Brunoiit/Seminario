c3ee0872317c7259e2c32c2d1cecb995e5b3cb4b
Brunoiit <francisco18car@gmail.com>
2023-05-28 22:39:47 -0500

    detalles

--- proyectofinal/admin/www_admin/panel/views.py
+++ proyectofinal/admin/www_admin/panel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+#crear la direccion de los templates para que funcione el index
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates")'
+)
+#definir el index
+def index(request):
+    return render(request, "index.html")
