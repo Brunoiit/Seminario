eb5263923bf624c5bca804fe391b9fab7b591203
Brunoiit <francisco18car@gmail.com>
2023-06-03 23:07:58 -0500

    modificaciones funcionales

--- proyectofinal/admin/www_admin/www_admin/settings.py
+++ proyectofinal/admin/www_admin/www_admin/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*etc4jkhyga3-cn*58$wz4wvyrqoc**@=!#i+bozwmd_)#bk9d'
+SECRET_KEY = 'django-insecure-r&mst9qg*g_@-8_)ozf@1vl(fzo8@z&$eoo9z$mg_b#xw%*hfa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'panel',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'America/Bogota'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
