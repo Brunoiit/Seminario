3061fd1a8e0857b84bbe8001f4808cdcb8d66d4f
Brunoiit <francisco18car@gmail.com>
2023-06-04 12:42:45 -0500

    Login y funcionalidades

--- proyectofinal/admin/www_admin/panel/urls.py
+++ proyectofinal/admin/www_admin/panel/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('cerrarSesion/', views.cerrarSesion, name="cerrarSesion"),
     path('', views.index, name='index'),
     path('ver', views.ver, name='ver'),
     path('agregar', views.agregar, name='agregar'),
-    path('modificar', views.modificar, name='modificar'),
-    path('eliminar', views.eliminar, name='eliminar')
+    path('modificar/<int:id_usr>/', views.modificar, name='modificar'),
+    path('eliminar/<int:id_usr>/', views.eliminar, name='eliminar')
 ]
 
--- proyectofinal/admin/www_admin/panel/views.py
+++ proyectofinal/admin/www_admin/panel/views.py
@@ -1,30 +1,43 @@
 from django.http import HttpResponse
 from django.urls import path
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from .models import Usuarios
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 from django.shortcuts import get_object_or_404, redirect
 from . import views
 
 def index(request):
     return render(request, "index.html")
 
+def cerrarSesion(request):
+    logout(request)
+    return redirect("/")
+
+@login_required
 def agregar(request):
-    if (request.method=="POST"):
-        if (request.POST.get("name") and request.POST.get("lastname") and request.POST.get("email") and request.POST.get("phone") and request.POST.get("password")):
-            user = Usuarios()
-            user.nombre_usr = request.POST.get("name")
-            user.apellido_usr = request.POST.get("lastname")
-            user.correo_usr = request.POST.get("email")
-            user.telefono_usr = request.POST.get("phone")
-            user.contrasena_usr = request.POST.get("password")
-            user.rol_usr = request.POST.get("rol")
-            user.save()
-            return redirect('ver')
-
+    if request.user.is_authenticated:
+        # Tu lógica aquí
+        if (request.method=="POST"):
+            if (request.POST.get("name") and request.POST.get("lastname") and request.POST.get("email") and request.POST.get("phone") and request.POST.get("password")):
+                user = Usuarios()
+                user.nombre_usr = request.POST.get("name")
+                user.apellido_usr = request.POST.get("lastname")
+                user.correo_usr = request.POST.get("email")
+                user.telefono_usr = request.POST.get("phone")
+                user.contrasena_usr = request.POST.get("password")
+                user.rol_usr = request.POST.get("rol")
+                user.save()
+                return redirect('ver')
+            return HttpResponse("Usuario autenticado")
+        else:
+            return render(request, "usuarios/agregar.html")
     else:
-        return render(request, "usuarios/agregar.html")
+        # En caso de que el usuario no esté autenticado
+        return HttpResponse("Usuario no autenticado")
+    
 
+@login_required
 def eliminar(request, id_usr):
     usuario = get_object_or_404(Usuarios, id_usr=id_usr)
     
@@ -34,24 +47,30 @@ def eliminar(request, id_usr):
     
     return render(request, "usuarios/eliminar.html", {'usuario': usuario})
 
+@login_required
 def ver(request):
     return render(request, "usuarios/ver.html", {'usuarios' : Usuarios.objects.all})
 
+@login_required
 def modificar(request, id_usr):
-    if request.method == "POST":
-        if request.POST.get("name") and request.POST.get("lastname") and request.POST.get("email") and request.POST.get("phone") and request.POST.get("password"):
-            user = Usuarios.objects.get(id_usr=id_usr)
-            user.nombre_usr = request.POST.get("name")
-            user.apellido_usr = request.POST.get("lastname")
-            user.correo_usr = request.POST.get("email")
-            user.telefono_usr = request.POST.get("phone")
-            user.contrasena_usr = request.POST.get("password")
-            user.rol_usr = request.POST.get("rol")
-            user.save()
-            return redirect("ver")
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if request.POST.get("name") and request.POST.get("lastname") and request.POST.get("email") and request.POST.get("phone") and request.POST.get("password"):
+                user = Usuarios.objects.get(id_usr=id_usr)
+                user.nombre_usr = request.POST.get("name")
+                user.apellido_usr = request.POST.get("lastname")
+                user.correo_usr = request.POST.get("email")
+                user.telefono_usr = request.POST.get("phone")
+                user.contrasena_usr = request.POST.get("password")
+                user.rol_usr = request.POST.get("rol")
+                user.save()
+                return redirect("ver")
+            return HttpResponse("Usuario autenticado")
+        else:
+            usuario = Usuarios.objects.get(id_usr=id_usr)
+            return render(request, "usuarios/modificar.html", {'usuario': usuario})
     else:
-        usuario = Usuarios.objects.get(id_usr=id_usr)
-        return render(request, "usuarios/modificar.html", {'usuario': usuario})
-
+        # En caso de que el usuario no esté autenticado
+        return HttpResponse("Usuario no autenticado")
 
 
