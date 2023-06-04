8988038576fd38187b0b509d96518f9c99c4eee6
Brunoiit <francisco18car@gmail.com>
2023-06-04 17:46:16 -0500

    Login funcionando

--- proyectofinal/admin/www_admin/panel/urls.py
+++ proyectofinal/admin/www_admin/panel/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/login/', views.login_view, name='login'),
     path('cerrarSesion/', views.cerrarSesion, name="cerrarSesion"),
     path('', views.index, name='index'),
     path('ver', views.ver, name='ver'),
--- proyectofinal/admin/www_admin/panel/views.py
+++ proyectofinal/admin/www_admin/panel/views.py
@@ -3,31 +3,25 @@ from django.urls import path
 from django.shortcuts import render, redirect
 from .models import Usuarios
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, redirect
 from . import views
-
-def index(request):
-    return render(request, "index.html")
-
-def cerrarSesion(request):
-    logout(request)
-    return redirect("/")
 
 @login_required
 def agregar(request):
     if request.user.is_authenticated:
-        # Tu lógica aquí
         if (request.method=="POST"):
             if (request.POST.get("name") and request.POST.get("lastname") and request.POST.get("email") and request.POST.get("phone") and request.POST.get("password")):
-                user = Usuarios()
-                user.nombre_usr = request.POST.get("name")
-                user.apellido_usr = request.POST.get("lastname")
-                user.correo_usr = request.POST.get("email")
-                user.telefono_usr = request.POST.get("phone")
-                user.contrasena_usr = request.POST.get("password")
-                user.rol_usr = request.POST.get("rol")
-                user.save()
+                usuario = Usuarios()
+                usuario.nombre_usr = request.POST.get("name")
+                usuario.apellido_usr = request.POST.get("lastname")
+                usuario.correo_usr = request.POST.get("email")
+                usuario.telefono_usr = request.POST.get("phone")
+                usuario.contrasena_usr = request.POST.get("password")
+                usuario.rol_usr = request.POST.get("rol")
+                usuario.save()
+                user = User.objects.create_user(username=request.POST.get("email"), email=request.POST.get("email"), password=request.POST.get("password"))
                 return redirect('ver')
             return HttpResponse("Usuario autenticado")
         else:
@@ -36,6 +30,25 @@ def agregar(request):
         # En caso de que el usuario no esté autenticado
         return HttpResponse("Usuario no autenticado")
     
+def index(request):
+    return render(request, "index.html")
+
+def cerrarSesion(request):
+    logout(request)
+    return redirect("/")
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('ver')
+        else:
+            return render(request, 'login.html', {'error_message': 'Credenciales inválidas'})
+    else:
+        return render(request, 'login.html')
 
 @login_required
 def eliminar(request, id_usr):
