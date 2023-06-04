c54e0a6a5e8ca5f14f62fa8ce7a915fc3d862e66
Brunoiit <francisco18car@gmail.com>
2023-06-04 01:40:17 -0500

    Creando las migraciones

--- proyectofinal/admin/www_admin/panel/models.py
+++ proyectofinal/admin/www_admin/panel/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Usuarios(models.Model):
+    id_usr = models.IntegerField(primary_key=True,)
+    nombre_usr = models.CharField(max_length=30, null=False)
+    apellido_usr = models.CharField(max_length=30, null=False)
+    correo_usr = models.CharField(max_length=50, null=False)
+    telefono_usr = models.IntegerField(null=True)
+    rol_usr = models.IntegerField(null=False)
+    fec_creacion_usr = models.DateTimeField(auto_now_add=True, null=True)
+    class meta:
+        db_table = 'Usuarios'
