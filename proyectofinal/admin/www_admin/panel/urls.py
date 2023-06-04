22c3401cb3803b7e40b1ad4f233c147b869854eb
Brunoiit <francisco18car@gmail.com>
2023-06-03 23:47:25 -0500

    Servidor de nuevo en linea

--- proyectofinal/admin/www_admin/panel/urls.py
+++ proyectofinal/admin/www_admin/panel/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from django import views
-#el . es de la misma carpeta (panel)
 from . import views
 
-#definir urlpatterns
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
--- proyectofinal/admin/www_admin/panel/views.py
+++ proyectofinal/admin/www_admin/panel/views.py
@@ -1,10 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-#crear la direccion de los templates para que funcione el index
-TEMPLATE_DIRS = (
-    'os.path.join(BASE_DIR, "templates")'
-)
-#definir el index
+from django.shortcuts import render
+
 def index(request):
-    return render(request, "index.html"),
+    return render(request, "index.html")
